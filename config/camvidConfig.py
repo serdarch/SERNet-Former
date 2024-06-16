27207106d760d598f3826e7584cabde367423dc7
Serdar Erişen <61043858+serdarch@users.noreply.github.com>
2024-06-16 12:00:53 -0600

    Add files via upload

---
+++ config/camvidConfig.py
@@ -0,0 +1,25 @@
+# @package training
+# Those arguments defines the training hyper-parameters
+epochs: 80
+num_workers: 1
+batch_size: 3
+shuffle: True
+cuda: 0
+precompute_multi_scale: False # Compute multiscate features on cpu for faster training / inference
+optim:
+	base_lr: 0.001
+	optimizer:
+	class: SGD
+	params:
+	lr: ${training.optim.base_lr}
+	lr_scheduler: ${lr_scheduler_v}
+	bn_scheduler:
+	bn_policy: "step_decay"
+	params: 
+	bn_momentum: 0.9
+	bn_decay: 0.95
+	decay_step: 10
+	bn_clip: 1
+weight_name: "latest" # can be named/changed according to the shared model weights
+enable_cudnn: False
+checkpoint_dir: "..."
