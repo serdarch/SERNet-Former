2c598d46e22cc2ec80beceafde795554226938fb
Serdar Erişen <61043858+serdarch@users.noreply.github.com>
2024-11-28 21:25:12 +0300

    Delete functions directory

--- functions/setup/load_configCamVid.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-def load_config(config_file):
-    with open(config_file, 'r') as f:
-        config = yaml.safe_load(f)
-    return config
-
-config = load_config('configCamVid.yaml')
